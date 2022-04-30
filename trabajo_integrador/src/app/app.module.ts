128594830a073d4930e8b068142a732e090d1140
alanriv01 <alanrivadeneira1991@gmail.com>
2022-04-30 06:58:13 -0300

    logos

--- trabajo_integrador/src/app/app.module.ts
+++ trabajo_integrador/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule
